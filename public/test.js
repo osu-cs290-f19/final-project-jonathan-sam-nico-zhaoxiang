c1be391d50a8f4a0dc19bb174cee667f414f23f3
Jonathan Macias <macijona@oregonstate.edu>
2019-12-10 10:39:13 -0800

    Added coloring div for review header div

--- public/test.js
+++ public/test.js
@@ -3,13 +3,21 @@ var as = document.getElementsByTagName('a');
 var title = document.getElementsByClassName('site-title');
 var colormode = document.getElementById('color-mode');
 var body = document.getElementsByTagName('body');
+var openbutton = document.getElementById("modal-close");
+var backdrop = document.getElementById('modal-backdrop');
+var modal = document.getElementById('review-modal');
+var cancel = document.getElementById('modal-cancel');
+var review = document.getElementById('add-review-button');
+var modal_body = document.getElementsByClassName('modal-body');
+
 for(var i =0; i<divs.length; i++){	
 	if(divs[i].classList.contains('upper-portion')||divs[i].classList.contains('lower-portion')||
-		divs[i].classList.contains('game-details-container')){
+			divs[i].classList.contains('game-details-container')||divs[i].classList.contains('modal-header')){
 		divs[i].classList.add('light2');
 	}
 	else{divs[i].classList.add('light');}
 }
+console.log(document.title);
 title[0].classList.add('light');
 colormode.addEventListener('change', function(event){
 	console.log(event.target.value);
@@ -22,7 +30,7 @@ colormode.addEventListener('change', function(event){
 		divs[i].classList.remove('light2');
 		if(event.target.value =="Light Mode"){
 			if(divs[i].classList.contains('upper-portion')||divs[i].classList.contains('lower-portion')||
-			divs[i].classList.contains('game-details-container')){
+			divs[i].classList.contains('game-details-container')||divs[i].classList.contains('modal-header')){
 				divs[i].classList.add('light2');
 			}
 			else{divs[i].classList.add('light');
@@ -33,7 +41,7 @@ colormode.addEventListener('change', function(event){
 		}
 		else if(event.target.value =="Dark Mode"){
 			if(divs[i].classList.contains('upper-portion')||divs[i].classList.contains('lower-portion')||
-				divs[i].classList.contains('game-details-container')){
+			divs[i].classList.contains('game-details-container')||divs[i].classList.contains('modal-header')){
 				divs[i].classList.add('dark2');
 			}
 			else{divs[i].classList.add('dark');
@@ -44,7 +52,7 @@ colormode.addEventListener('change', function(event){
 		}
 		else{
 			if(divs[i].classList.contains('upper-portion')||divs[i].classList.contains('lower-portion')||
-			divs[i].classList.contains('game-details-container')){
+			divs[i].classList.contains('game-details-container')||divs[i].classList.contains('modal-header')){
 				divs[i].classList.add('osu2');
 			}
 			else{divs[i].classList.add('osu');
@@ -55,3 +63,21 @@ colormode.addEventListener('change', function(event){
 		}
 	}
  });
+
+//console.log(body[0].classList[0]);
+if(document.title =="Reviews"){
+	openbutton.addEventListener('click', function(event){
+		modal_body[0].classList.add('neutral-color');
+		modal.classList.remove('hidden');
+		backdrop.classList.remove('hidden');
+		})
+	cancel.addEventListener('click', function(event){
+		modal.classList.add('hidden');
+		backdrop.classList.add('hidden');
+	})
+	review.addEventListener('click', function(event){
+		modal.classList.add('hidden');
+		backdrop.classList.add('hidden');	
+	});
+}
+
